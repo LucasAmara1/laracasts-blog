afa433c91812e9bee6b3bc52c5a57133e77c6d3c
Lucas_Amara1 <lucas.amaral@checklistfacil.com>
2021-09-08 15:48:11 -0300

    Adição de formulários e autenticação

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +18,11 @@ use App\Models\User;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
