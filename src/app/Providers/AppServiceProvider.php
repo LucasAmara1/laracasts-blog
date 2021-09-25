b02f8afff70fcb0454308d4f3467a870d9ec3acb
Lucas_Amara1 <lucas.amaral@checklistfacil.com>
2021-09-24 22:34:28 -0300

    Adição de opção para eitar e excluir posts na sessão de administrador

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use App\Services\Newsletter;
-use App\Services\MailchimpNewsletter;
 use MailchimpMarketing\ApiClient;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Gate;
+use App\Services\MailchimpNewsletter;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -33,6 +36,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'Amara1';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin');
+        });
     }
 }
